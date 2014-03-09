3a6e5f0ca1b4f24fcf5ae802ef464351b47faa67
deyshupor <hardeyshupor@yahoo.com>
2014-03-09 22:46:58 +0100

    Initial Project files

---
+++ hurmac/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(hurmac.Startup))]
+namespace hurmac
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
